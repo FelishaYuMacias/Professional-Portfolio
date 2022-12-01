c54d82ad1a7d950c7eb205d1a55e36d3e5de4c0d
Felisha <felisha.j.yu@gmail.com>
2022-12-01 13:59:38 -0800

    added basic file paths

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import NavTabs from './NabTabs';
+import Home from './Home';
+import About from './About';
+import Resume from './Resume';
+import Contact from './Contact';
+import Project from './Project';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Project') {
+        return <Project />;
+      }
+    return <Home />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}

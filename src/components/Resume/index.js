1a97020323ca1e29c90cf4eb148bd3e0441dba24
Felisha <felisha.j.yu@gmail.com>
2022-12-02 10:51:53 -0800

    start resume edit

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import './assets/style.css';
 
 export default function Resume() {
   return (
     <div>
-      <h1>Resume Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+      <h1>Resume</h1>
+      <div className='skills'>
+        <div>
+        <p>
+        Download my Resume<a href="https://docs.google.com/document/d/e/2PACX-1vQ5EqdbCSgbwZuIJxolrB7gsEV-nPsiqvnl4fnSl1g4N2rj54WJ2vkTwAqRD0cGZf7j4HwGXkTxh-Dx/pub" target={"_blank"}></a>
+        </p>
+        </div>
+        <div><h3>Front-end Proficiencies</h3>
+        <ul className="list-unstyled">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Handlebars</li>
+          <li>Progressive Web Applications</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+        </div>
+      <div>
+        <h3>Back-end Proficiencies</h3>
+        <ul className="list-unstyled">
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Object Oriented Programming</li>
+          <li>Express</li>
+          <li>Object Relational Mapping</li>
+          <li>Model View Controller</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+        </ul>
+        </div>
+      </div>
+      </div>
   );
 }

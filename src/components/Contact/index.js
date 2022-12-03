4e997f45ceb7addece594c18af0f3bc2b9f8a8ea
Felisha <felisha.j.yu@gmail.com>
2022-12-02 16:32:20 -0800

    contact page complete

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './assets/style.css';
+
 
 export default function Contact() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { name, email, message } = formState; 
+
+  
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  const handleEmpty = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: " "});
+      console.log('Handle Form', formState);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <form className='mx-auto' id="contact-form" onSubmit={handleSubmit}>
+      <input className="form-control"  type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleEmpty} placeholder="Name"/>
+  <div className="form-group">
+    <input type="email" 
+    name="email"
+    defaultValue={email}
+    onBlur={handleEmpty}className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+  </div>
+  <div className="form-group">
+    <textarea className="form-control" 
+    name="message"
+    defaultValue={message}
+    onBlur={handleEmpty}id="exampleFormControlTextarea1" rows="3" placeholder="Write your message here"></textarea>
+  </div>
+  {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+  <button onClick={handleSubmit} type="submit" className="btn">Submit</button>
+</form>
     </div>
   );
 }

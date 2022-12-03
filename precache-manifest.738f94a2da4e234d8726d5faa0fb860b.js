f8baf78898a77eeb41f826b5705ffa41fee8d8d5
Felisha <felisha.j.yu@gmail.com>
2022-12-02 19:03:59 -0800

    Updates

---
+++ precache-manifest.738f94a2da4e234d8726d5faa0fb860b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9aafbf467463db837ec",
+    "url": "/Professional-Portfolio/static/css/main.baeb4f18.chunk.css"
+  },
+  {
+    "revision": "b9aafbf467463db837ec",
+    "url": "/Professional-Portfolio/static/js/main.b9aafbf4.chunk.js"
+  },
+  {
+    "revision": "b90b79f2adc624419e7c",
+    "url": "/Professional-Portfolio/static/css/1.57f925e4.chunk.css"
+  },
+  {
+    "revision": "b90b79f2adc624419e7c",
+    "url": "/Professional-Portfolio/static/js/1.b90b79f2.chunk.js"
+  },
+  {
+    "revision": "1631df14c421b0ee9f8f",
+    "url": "/Professional-Portfolio/static/js/runtime~main.1631df14.js"
+  },
+  {
+    "revision": "bdbfda448d27f878dc203475b911032d",
+    "url": "/Professional-Portfolio/static/media/felisha.bdbfda44.jpg"
+  },
+  {
+    "revision": "b063eec325cc73979094d00c7dda70eb",
+    "url": "/Professional-Portfolio/static/media/family.b063eec3.jpg"
+  },
+  {
+    "revision": "fb8f94d743eccc31007531babcf9bee9",
+    "url": "/Professional-Portfolio/static/media/resume.fb8f94d7.png"
+  },
+  {
+    "revision": "d13d114909e33134ce08d2444162b7c3",
+    "url": "/Professional-Portfolio/static/media/cover-image.d13d1149.jpg"
+  },
+  {
+    "revision": "cdacab359c52c1b7d9c646936731f94e",
+    "url": "/Professional-Portfolio/static/media/zoo-animal-game.cdacab35.png"
+  },
+  {
+    "revision": "ded35d207fa5e1a5d7e909717ed6b785",
+    "url": "/Professional-Portfolio/static/media/wish-list.ded35d20.png"
+  },
+  {
+    "revision": "8accd57ef38441266cfe9f9ed189c4c4",
+    "url": "/Professional-Portfolio/static/media/note-taker.8accd57e.png"
+  },
+  {
+    "revision": "ebc3d80859ac1ae1cc8bc8323edcdc83",
+    "url": "/Professional-Portfolio/static/media/shiny-hunter.ebc3d808.jpg"
+  },
+  {
+    "revision": "ab814cd9b4c72e30a7eb5b7172317c66",
+    "url": "/Professional-Portfolio/static/media/tech.ab814cd9.jpg"
+  },
+  {
+    "revision": "7e36a21d0831995a5c11cbaa7b484c5b",
+    "url": "/Professional-Portfolio/static/media/timed-quiz.7e36a21d.jpg"
+  },
+  {
+    "revision": "767ea3bf4c2584e5dcc5272fc23cbc3a",
+    "url": "/Professional-Portfolio/index.html"
+  }
+];
